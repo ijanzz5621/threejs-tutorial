6e2121ae4883d6c4f1b4acc4b56f123580c599de
Mohd Redzuan, Sharizan <Sharizan.MohdRedzuan@hess.com>
2024-10-25 22:53:20 +0800

    Update ThreeJS and Animation

---
+++ src/js/animations/basic_acceleration.js
@@ -0,0 +1,44 @@
+const ballRadius = 50;
+const ball = {
+    x: 0 + ballRadius,
+    y: 0 + ballRadius,
+    radius: ballRadius,
+    color: "#00FF00",
+    dx: 2, // Velocity in x direction
+    dy: 2  // Velocity in y direction
+  };
+
+export function drawBall(ctx) {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = ball.color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+export function resetBall() {
+    ball.x = 0 + ballRadius;
+    ball.y = 0 + ballRadius;
+}
+
+export function ballAnimationUpdate(canvas, ctx, animation) {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall(ctx); // Draw the ball
+  
+    // Update ball position
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+  
+    // Check for wall collisions
+    if (ball.x + ball.radius > canvas.width || ball.x - ball.radius < 0) {
+      ball.dx = -ball.dx;
+    }
+    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+      ball.dy = -ball.dy;
+    }
+  
+    // Request the next frame
+    animation = requestAnimationFrame(ballAnimationUpdate); 
+}
+  
---
+++ src/js/scripts-animation-learning.js
@@ -0,0 +1,61 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+import { resetBall, ballAnimationUpdate } from '../js/animations/basic_acceleration.js';
+
+var currentAnimationType = "Basic Acceleration";
+var timeOutFunctionId;
+var animation = undefined;
+
+// Events Listener
+window.addEventListener('resize', resetCanvas);
+document.getElementById('animationType').addEventListener('change', function(event) {
+  const selVal = event.target.value;
+
+  if (selVal != currentAnimationType) {
+    currentAnimationType = selVal;
+  }
+
+  resetCanvas();
+
+});
+
+// Resize canvas & Animate
+function resetCanvas() {
+
+    // clearTimeOut() resets the setTimeOut() timer
+    // due to this the function in setTimeout() is 
+    // fired after we are done resizing
+    clearTimeout(timeOutFunctionId);
+  
+    // setTimeout returns the numeric ID which is used by
+    // clearTimeOut to reset the timer
+    timeOutFunctionId = setTimeout(workAfterResizeIsDone, 500);
+}
+
+function workAfterResizeIsDone() {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  const content = document.getElementById("content");
+  const contentInfo = content.getBoundingClientRect();
+  canvas.width = contentInfo.width;
+  canvas.height = contentInfo.height;   
+  
+  if (animation !== undefined) {
+    window.cancelAnimationFrame(animation);
+  }
+
+  if (currentAnimationType === "Basic Acceleration") {
+    resetBall()    
+    ballAnimationUpdate(canvas, ctx, animation);
+  } else {
+    alert("Animation for " + currentAnimationType + " is not available yet");
+  }
+}
+
+// Start Here
+resetCanvas(); // Initial resize
+
+
+
